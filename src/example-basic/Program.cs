2ca0ec48503d9ca95e0918354cba8f5c5ea62b4b
amunra <amunra@users.noreply.github.com>
2022-08-17 17:57:09 +0100

    docs: Examples (#8)
    * docs: Added basic and auth-tls examples.
    
    * chore: Added CI.
    
    * chore: Added CODEOWNERS.
    
    * chore: Hiding Visual Studio Backup directory in .gitignore.
    
    * test: Fixed tests on Windows.
    
    * doc: Added a link to the QuestDB .NET library on NuGet.
    
    * chore: Fixing broken examples.manifest.yaml auth substitution string.

---
+++ src/example-basic/Program.cs
@@ -0,0 +1,32 @@
+﻿using QuestDB;
+using System;
+using System.Threading.Tasks;
+
+namespace QuestDBDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var sender = await LineTcpSender.ConnectAsync(
+                "localhost",
+                9009,
+                tlsMode: TlsMode.Disable);
+            sender.Table("trades")
+                .Symbol("pair", "USDGBP")
+                .Symbol("type", "buy")
+                .Column("traded_price", 0.83)
+                .Column("limit_price", 0.84)
+                .Column("qty", 100)
+                .Column("traded_ts", new DateTime(
+                    2022, 8, 6, 7, 35, 23, 189, DateTimeKind.Utc))
+                .At(DateTime.UtcNow);
+            sender.Table("trades")
+                .Symbol("pair", "GBPJPY")
+                .Column("traded_price", 135.97)
+                .Column("qty", 400)
+                .AtNow();
+            await sender.SendAsync();
+        }
+    }
+}
